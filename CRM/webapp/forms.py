d7170c0950ec093c2394c6398ac0459e72bbb0f2
shriharimrao <93112966+shriharimrao@users.noreply.github.com>
2024-01-18 12:07:56 +0530

    login form

--- CRM/webapp/forms.py
+++ CRM/webapp/forms.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 
 from django import forms
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput,TextInput
+
 #Register a user
 class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','password1','password2']
+#login a user
+class LoginForm(AuthenticationForm):
+    username=forms.CharField(widget=TextInput())
+    password=forms.CharField(widget=PasswordInput())
